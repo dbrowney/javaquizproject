e7c41183f21521f0d2d358369b97c931d2326900
Browney <Browney@10.0.0.86>
2020-05-16 22:36:12 -0400

    update1

--- Java Quiz Project/src/Main.java
+++ Java Quiz Project/src/Main.java
@@ -1,9 +1,30 @@
 
 public class Main {
+  // Author: Donald Browney
+  /*
+   * This is a quiz about Java programming language which can be used to help study for
+   * certification
+   */
+  // Built-In Functions
+  // int
+  // float
+  // double
+  // char
+  //
 
   public static void main(String[] args) {
-    String quizQuestion1;
 
+    String quizQuestion1 = "JDK stands for Java Development Kit. TRUE or FALSE";
+    boolean correctAnswer = true;
+    int numCorrect = 0;
+    double pointsPerCorrectAnswer = 14.28571;
+
+    System.out.println("Hello and welcome to JAVA QUIZ 1.0 - INITIAL SUCCESS OR TOTAL FAILURE");
+    System.out.println(
+        "This quiz is intended to sharpen your skills and bring you 1 step closer to certification.");
+    System.out.println("Let's get started");
+    System.out.println(quizQuestion1);
+    
   }
 
 }
