1917af59b6c5fea0a0ad807c68866932e3666668
Browney <Browney@10.0.0.86>
2020-05-18 16:36:17 -0400

    update 3 - final code for milestone 1

--- Java Quiz Project/src/Main.java
+++ Java Quiz Project/src/Main.java
@@ -1,11 +1,10 @@
-import java.util.Scanner;
-
+// COP 2006 Integration Project - Summer A 2020
 // Author: Donald Browney
 /*
- * This is a quiz about Java programming language which can be used to help study for certification
+ * This is a quiz about Java programming language which can be used to help study for certification.
  */
 // Coding help provided by COP 2006 lessons, W3Schools.com, and Oracle Java lessons unless otherwise
-// noted
+// noted.
 // Built-In Data Types
 // byte: whole number between -128 and 127, 8 bit
 // int: whole number between -2^31 and 2^31-1, 32 bit
@@ -15,6 +14,8 @@ import java.util.Scanner;
 // double: decimal number, 64 bit, default decimal data type
 // char: a single character or letter enclosed by single quotes, 16 bit
 // boolean: only possible values are true and false, 1 bit of info
+
+import java.util.Scanner;
 
 public class Main {
 
@@ -32,33 +33,31 @@ public class Main {
 
     System.out.println("Hello and welcome to JAVA QUIZ 1.0 - INITIAL SUCCESS OR TOTAL FAILURE");
     System.out.println(
-        "This quiz is intended to sharpen your skills and bring you 1 step closer to certification.");
-    System.out.println();
-    System.out.println("Let's get started");
-    System.out.println();
+        "This quiz is intended to sharpen your skills and bring you 1 step closer to certification.\n");
+    System.out.println("Let's get started\n");
     System.out.print(quizQuestion1);
-    System.out.println("Enter TRUE or FALSE");
-    Scanner scan = new Scanner(System.in);
+    System.out.println("Enter TRUE or FALSE"); // User Prompt
+    Scanner scan = new Scanner(System.in); // used to gather user input to questions. not sure if
+                                           // I'm supposed to close 'scan' out
     String answer1 = scan.nextLine();
     if (correctAnswer1 == Boolean.parseBoolean(answer1)) {
       // String to Boolean: https://www.java67.com/2018/03/java-convert-string-to-boolean.html
-      System.out.println("Correct");
+      System.out.println("Correct\n");
       numCorrect++;
     } else {
-      System.out.println("Incorrect");
+      System.out.println("Incorrect\n");
     }
-    System.out.println();
     System.out.print(quizQuestion2);
-    System.out.println("Enter TRUE or FALSE");
+    System.out.println("Enter TRUE or FALSE"); // User Prompt
     String answer2 = scan.nextLine();
     if (correctAnswer2 == Boolean.parseBoolean(answer2)) {
-      System.out.println("Correct");
+      // String to Boolean: https://www.java67.com/2018/03/java-convert-string-to-boolean.html
+      System.out.println("Correct\n");
       numCorrect++;
     } else {
-      System.out.println("Incorrect");
+      System.out.println("Incorrect\n");
     }
     double totalPoints = numCorrect * pointsPerCorrectAnswer;
-    System.out.println();
     System.out.println("Total Points: " + totalPoints);
 
   }
