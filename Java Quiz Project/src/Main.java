eae5e89b706d5a62dce8038496e30911835b8efe
Browney <Browney@10.0.0.86>
2020-06-01 00:09:33 -0400

    update for Sprint 2

--- Java Quiz Project/src/Main.java
+++ Java Quiz Project/src/Main.java
@@ -16,50 +16,117 @@
 // boolean: only possible values are true and false, 1 bit of info
 
 import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
 
   public static void main(String[] args) {
-
-    String quizQuestion1 = "JDK stands for Java Development Kit. ";
-    String quizQuestion2 = "A variable is a location in memeory. ";
+    // VARIABLES
+    Scanner scan = new Scanner(System.in); // used to gather user input to questions. not sure if
+    // I'm supposed to close 'scan' out
+    Random randomDifficulty = new Random();
+    String[] easyQuestions = {"JDK stands for Java Development Kit. (Enter TRUE or FALSE)",
+        "A variable is a location in memeory. (Enter TRUE or FALSE)",
+        "How many bits are in a byte?\na: 2\nb: 4\nc: 6\nd: 8"};
+    String[] mediumQuestions = {
+        "In object oriented programming, an object comprises of properties and behaviors where "
+            + "properties represented as fields of the object and behavior is represented as method. "
+            + "(Enter TRUE or FALSE)",
+        "Which of the following language is called a procedural language?\na: Java\nb: C\nc: Java "
+            + "C\nd: C++",
+        "A character preceded by backslash is called an escape sequence. (Enter TRUE or FALSE)"};
+    String[] hardQuestions = {
+        "The Scanner class accepts input in which form?\na: Callables\nb: Future\nc: Tokens\nd: "
+            + "Integer",
+        "The classes of the Java class library are organized into packages. (Enter TRUE or FALSE)",
+        "In the AND (&&) test, if the first expression on the left hand side is false, then there "
+            + "is no need to evaluate the second statement. (Enter TRUE or FALSE)"};
+    String[] easyAnswers = {"true", "true", "d"};
+    String[] mediumAnswers = {"true", "b", "true"};
+    String[] hardAnswers = {"c", "true", "true"};
     // Scope means a variable is only accessible in the part of the program in which it was defined
-    boolean correctAnswer1 = true;
-    boolean correctAnswer2 = true;
     int numCorrect = 0; // numCorrect is a variable which is a location in memory
-    final double POINTS_PER_CORRECT_ANSWER = 14.28571; // pointsPerCorrectAnswer is a final variable
-                                                    // which means it's value is constant and cannot
-                                                    // be changed
+    final double POINTS_PER_CORRECT_ANSWER = 14.28571; // POINTS_PER_CORRECT_ANSWER is a final
+                                                       // variable which means it's value is
+                                                       // constant and cannot be changed
+    boolean restartQuiz = true;
+    int difficultyLevel;
+    int counter;
+    double totalPoints;
+    String userInput;
 
+    // INTRO
     System.out.println("Hello and welcome to JAVA QUIZ 1.0 - INITIAL SUCCESS OR TOTAL FAILURE");
     System.out.println(
         "This quiz is intended to sharpen your skills and bring you 1 step closer to certification.\n");
-    System.out.println("Let's get started\n");
-    System.out.print(quizQuestion1);
-    System.out.println("Enter TRUE or FALSE"); // User Prompt
-    Scanner scan = new Scanner(System.in); // used to gather user input to questions. not sure if
-                                           // I'm supposed to close 'scan' out
-    String answer1 = scan.nextLine();
-    if (correctAnswer1 == Boolean.parseBoolean(answer1)) {
-      // String to Boolean: https://www.java67.com/2018/03/java-convert-string-to-boolean.html
-      System.out.println("Correct\n");
-      numCorrect++;
-    } else {
-      System.out.println("Incorrect\n");
-    }
-    System.out.print(quizQuestion2);
-    System.out.println("Enter TRUE or FALSE"); // User Prompt
-    String answer2 = scan.nextLine();
-    if (correctAnswer2 == Boolean.parseBoolean(answer2)) {
-      // String to Boolean: https://www.java67.com/2018/03/java-convert-string-to-boolean.html
-      System.out.println("Correct\n");
-      numCorrect++;
-    } else {
-      System.out.println("Incorrect\n");
-    }
-    double totalPoints = numCorrect * POINTS_PER_CORRECT_ANSWER;
-    System.out.println("Total Points: " + totalPoints);
 
+    // QUIZ
+    do {
+      counter = 0;
+      System.out
+          .println("To get started, please tell me the difficulty level you would like to try.\n");
+      System.out.println("1: Easy");
+      System.out.println("2: Medium");
+      System.out.println("3: Hard");
+      difficultyLevel = scan.nextInt();
+      scan.nextLine();
+      switch (difficultyLevel) {
+        case 1:
+          while (counter <= easyQuestions.length - 1) {
+            System.out.println(easyQuestions[counter]);
+            String userAnswer = scan.nextLine();
+            if (userAnswer.equalsIgnoreCase(easyAnswers[counter])) {
+              System.out.println("Correct\n");
+              numCorrect += 1;
+            } else {
+              System.out.println("Incorrect\n");
+            }
+            counter += 1;
+          }
+          break;
+        case 2:
+          while (counter <= mediumQuestions.length - 1) {
+            System.out.println(mediumQuestions[counter]);
+            String userAnswer = scan.nextLine();
+            if (userAnswer.equalsIgnoreCase(mediumAnswers[counter])) {
+              System.out.println("Correct\n");
+              numCorrect += 1;
+            } else {
+              System.out.println("Incorrect\n");
+            }
+            counter += 1;
+          }
+          break;
+        case 3:
+          while (counter <= hardQuestions.length - 1) {
+            System.out.println(hardQuestions[counter]);
+            String userAnswer = scan.nextLine();
+            if (userAnswer.equalsIgnoreCase(hardAnswers[counter])) {
+              System.out.println("Correct\n");
+              numCorrect += 1;
+            } else {
+              System.out.println("Incorrect\n");
+            }
+            counter += 1;
+          }
+          break;
+        default:
+          System.out.println("random");
+      }
+
+      // QUIZ SCORE
+      totalPoints = numCorrect * POINTS_PER_CORRECT_ANSWER;
+      System.out.printf("%s%f", "Total Points: ", totalPoints);
+
+      // RESTART QUIZ
+      System.out.println("\nWould you like to take the quiz again? (Enter true or false)");
+      userInput = scan.nextLine();
+      if (userInput.equalsIgnoreCase("true")) {
+        restartQuiz = true;
+      } else {
+        restartQuiz = false;
+      }
+    } while (restartQuiz == true);
+    System.out.println("Thank you for taking my quiz and Good Luck on getting your certification!");
   }
-
 }
